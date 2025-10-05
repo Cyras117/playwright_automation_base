fd68e2c16013a0db5322dbfa4980d6dcbc458f9f
Cyras117 <alexsandro.oliveira117@gmail.com>
2025-10-05 00:46:34 -0400

    Changing browsers and devices to parametrized

--- conftest.py
+++ conftest.py
@@ -1,6 +1,13 @@
 import pytest
 from collections.abc import AsyncGenerator
-from playwright.async_api import async_playwright,Playwright,Browser,Page
+from playwright.async_api import async_playwright,Playwright,Browser,Page,BrowserContext
+
+
+
+ALL_BROWSERS = ["chromium", "firefox", "webkit"]
+ALL_DEVICES = [None, "iPhone 12", "Pixel 5", "Galaxy S24"]
+ALL_CHANNELS = ["chrome","msedge"]
+
 
 
 def pytest_addoption(parser):
@@ -11,7 +18,7 @@ def pytest_addoption(parser):
         help="Browser channel to use (e.g., chrome, msedge). Only valid with chromium."
     )
 
-def pytest_configure(config:pytest.Config):
+def pytest_configure():
     print("✅ pytest_configure executed (conftest.py detected)")
 
 @pytest.fixture(scope="session")
@@ -20,48 +27,28 @@ async def playwright_instance():
         yield pw
 
 @pytest.fixture(scope="session")
-async def browser(playwright_instance:Playwright,pytestconfig:pytest.Config):
-    browser_name = pytestconfig.getoption("browser")
+async def page(playwright_instance:Playwright,pytestconfig:pytest.Config,request):
+    #Get headed param
     headed = False if pytestconfig.getoption("headed") else True
-    browser_channel = pytestconfig.getoption("channel")
-    device_name= pytestconfig.getoption("device")
-    
-    if browser_name:
-        browser_name = browser_name[0]
-        if browser_name == "chromium" and browser_channel:
-            browser_type = getattr(playwright_instance, browser_name)
-            browser = await browser_type.launch(channel=browser_channel, headless=headed)
-            yield browser
-            await browser.close()
-        else:
-            browser_type = getattr(playwright_instance, browser_name)
-            browser = await browser_type.launch(headless=headed)
-            yield browser
-            await browser.close()
-    elif device_name:
-        device = playwright_instance.devices[device_name]
-        browser_type = getattr(playwright_instance, device['default_browser_type'])
-        browser = await browser_type.launch(headless=headed)
-        yield browser
-        await browser.close()
-    else:
-        print("Invalid browser/device args...")
 
+    #Seting browser context
+    context: BrowserContext
+    browser_name = request.param.get("browser")
+    device_name = request.param.get("device")
+    browser_type = getattr(playwright_instance, browser_name)
+    browser: Browser = await browser_type.launch(headless=headed)
 
-@pytest.fixture
-async def page(browser:Browser,playwright_instance:Playwright,pytestconfig:pytest.Config)-> AsyncGenerator[Page, None]:
-    context = None
-    device_name= pytestconfig.getoption("device")
+    #checking if there is device
     if device_name:
         device = playwright_instance.devices[device_name]
         context = await browser.new_context(**device)
-        page = await context.new_page()
-        yield page
-        await context.close()
     else:
         context = await browser.new_context()
-        page = await context.new_page()
-        yield page
-        await context.close()
 
+    page:Page = await context.new_page()
 
+    #Yield page for the test
+    yield page
+    await context.close() 
+
+    
--- tests/test_home.py
+++ tests/test_home.py
@@ -6,13 +6,20 @@ from playwright.async_api import Page
 @allure.story("Valid credentials")
 @allure.severity(allure.severity_level.CRITICAL)
 @pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "page",
+    [{"browser": b, "device": d} for b in ["chromium", "firefox", "webkit"] for d in [None, "iPhone 12", "Pixel 5"]],
+    indirect=True,
+)
 async def test_example(page:Page):
     pg = HomePage(page)
     await pg.open()
 
 @pytest.mark.asyncio
+@pytest.mark.parametrize("page",[{"browser":b,"device":None,"channel":None}for b in ["chromium", "firefox", "webkit"]], indirect=True)
 async def test_dois(page:Page):
     await HomePage(page).open()
     await HomePage(page).go_to_docs()
     title = await HomePage(page).page.title()
     assert "Playwright" in title
+    
